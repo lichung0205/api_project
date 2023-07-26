8153a927d152ebf1894326ad15c46538dffa5f22
neilfong <lichung0205@gmail.com>
2023-07-26 12:19:12 +0800

    增加登入頁、調整格式化自動將雙引號變成單引號

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,10 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
+  {
+    path: '/login',
+    component: () => import('../views/LoginPage.vue'),
+  },
 ];
 
 const router = createRouter({
